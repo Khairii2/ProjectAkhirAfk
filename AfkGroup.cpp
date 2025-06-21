3d8e99862c53a45aa4ee868b2b8e99e8e66835c1
Khairii2 <jw.miul28@gmail.com>
2025-06-21 14:43:29 +0800

    Menambahkan pembersih layar dan struktur node tiket

--- AfkGroup.cpp
+++ AfkGroup.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+// Menambah pembersih layar terminal atau console
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
+
+// Menambahkan struktur node tiket
+struct Tiket {
+    string namaPembeli;
+    string judulFilm;
+    int jumlahTiket;
+    int hargaPerTiket;
+    Tiket* next;
+};
